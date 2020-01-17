a3985522df8ecdf2a59b375ab0b4d666bed903cf
黎伟航 <lwh1094464365@163.com>
2020-01-17 17:30:58 +0800

    更换favicon图标

--- src/main.js
+++ src/main.js
@@ -6,14 +6,12 @@ import router from './router'
 import Vant from 'vant'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-// eslint-disable-next-line camelcase
-import global_variable from './api/global_variable'
+import globalVariable from './api/global_variable'
 
 Vue.use(Vant)
 Vue.use(VueAxios, axios)
 
-// eslint-disable-next-line camelcase
-Vue.prototype.GLOBAL = global_variable
+Vue.prototype.GLOBAL = globalVariable
 
 Vue.config.productionTip = false
 
